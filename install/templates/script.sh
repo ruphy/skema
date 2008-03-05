cef5a176f4fb1c1e945fb3e82882f893c2303023
Riccardo Iaconelli <riccardo@kde.org>
2008-03-05 20:54:00 +0100

    Install LGPL and plasma-applet
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- install/templates/script.sh
+++ install/templates/script.sh
@@ -3,5 +3,7 @@ cp -a ../skema.rb "<%= File.join(@install_prefix, 'bin/skema') %>"
 cp -a skemarc "<%= File.join(ENV['HOME'], '.skemarc') %>" 
 mkdir -p "<%= File.join(@install_prefix, 'share/skema') %>"
 cp -a ../gpl "<%= File.join(@install_prefix, 'share/skema/') %>"
+cp -a ../lgpl "<%= File.join(@install_prefix, 'share/skema/') %>"
 cp -a ../kapp "<%= File.join(@install_prefix, 'share/skema/') %>"
+cp -a ../plasma-applet "<%= File.join(@install_prefix, 'share/skema/') %>"
 
