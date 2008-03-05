25f55159ab984de33195165c6fdd99407b3fc8e4
Riccardo Iaconelli <riccardo@kde.org>
2008-03-05 22:18:03 +0100

    Add CMakeLists.txt and source files for the plasma applet. now it should work flawlessly.
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ plasma-applet/templates/plasma-app.cpp
@@ -0,0 +1,62 @@
+<% filename "#{@slug}.cpp" %> 
+#include "<%= @slug %>.h"
+#include <QPainter>
+#include <QFontMetrics>
+#include <QSizeF>
+ 
+#include <plasma/svg.h>
+#include <plasma/theme.h>
+ 
+<%= @slug.uppercase %>::<%= @slug.uppercase %>(QObject *parent, const QVariantList &args)
+    : Plasma::Applet(parent, args),
+    m_svg("widgets/background", this),
+    m_icon("plasma")
+{
+    // this will get us the standard applet background, for free!
+    setDrawStandardBackground(true);
+    resize(200, 200);
+}
+ 
+ 
+<%= @slug.uppercase %>::~<%= @slug.uppercase %>()
+{
+    if (failedToLaunch()) {
+        // Do some cleanup here
+    } else {
+        // Save settings
+    }
+}
+ 
+void <%= @slug.uppercase %>::init()
+{
+    m_svg.setContentType(Plasma::Svg::SingleImage);
+ 
+  
+    // A small demonstration of the setFailedToLaunch function
+    if (m_icon.isNull()) {
+        setFailedToLaunch(true, "No world to say hello");
+    }
+} 
+ 
+ 
+void <%= @slug.uppercase %>::paintInterface(QPainter *p,
+        const QStyleOptionGraphicsItem *option, const QRect &contentsRect)
+{
+    p->setRenderHint(QPainter::SmoothPixmapTransform);
+    p->setRenderHint(QPainter::Antialiasing);
+ 
+    // Now we draw the applet, starting with our svg
+    m_svg.resize((int)contentsRect.width(), (int)contentsRect.height());
+    m_svg.paint(p, (int)contentsRect.left(), (int)contentsRect.top());
+ 
+    // We place the icon and text
+    p->drawPixmap(7, 0, m_icon.pixmap((int)contentsRect.width(),(int)contentsRect.width()-14));
+    p->save();
+    p->setPen(Qt::white);
+    p->drawText(contentsRect,
+                Qt::AlignBottom | Qt::AlignHCenter,
+                "Hello Plasmoid!");
+    p->restore();
+}
+ 
+#include "<%= @slug %>.moc"
---
+++ plasma-applet/templates/plasma-app.h
@@ -0,0 +1,34 @@
+<% filename "#{@slug}.h" %> // Here we avoid loading the header multiple times
+#ifndef <%= @slug.uppercase %>_HEADER
+#define <%= @slug.uppercase %>_HEADER
+// We need the Plasma Applet headers
+#include <KIcon>
+ 
+#include <Plasma/Applet>
+#include <Plasma/Svg>
+ 
+class QSizeF;
+ 
+// Define our plasma Applet
+class <%= @slug.capitalize %> : public Plasma::Applet
+{
+    Q_OBJECT
+    public:
+        // Basic Create/Destroy
+        <%= slug.capitalize %>(QObject *parent, const QVariantList &args);
+        ~<%= slug.capitalize %>();
+ 
+        // The paintInterface procedure paints the applet to screen
+        void paintInterface(QPainter *painter,
+                const QStyleOptionGraphicsItem *option,
+                const QRect& contentsRect);
+        void init();
+ 
+    private:
+        Plasma::Svg m_svg;
+        KIcon m_icon;
+};
+ 
+// This is the command that links your applet to the .desktop file
+K_EXPORT_PLASMA_APPLET(<%= @slug %>, PlasmaTutorial1)
+#endif
