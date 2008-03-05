f5914e63750993208caef886d33d011ca825761a
Riccardo Iaconelli <riccardo@kde.org>
2008-03-05 20:41:21 +0100

    Add first skeleton to generate a plasma applet.
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

---
+++ plasma-applet/plasma-applet.rb
@@ -0,0 +1,8 @@
+template :slug => nil,
+         :name => nil,
+         :title => nil,
+         :description => nil,
+         :version => "0.1",
+         :author => nil,
+         :email => nil
+
